7056c0332dea6983c08f961e8f1c2f3e43ba3287
Abhishek Punia <akpunia@berkeley.edu>
2017-09-29 19:25:16 -0700

    Fixed Contract

--- contracts/CarInsurance.sol
+++ contracts/CarInsurance.sol
@@ -1,7 +1,7 @@
-pragma solidity ^0.4.0;
+solidity 0.4.17;
 contract CarInsurance {
     
-    enum Location{ longtitute, latitude  }
+    //enum Location{ longtitute, latitude  }
     enum ActionChoices { GoLeft, GoRight, GoStraight, GoBack}
     
     uint private idGen = 1;
@@ -70,11 +70,87 @@ contract CarInsurance {
         return totalDistance * price;
     }
     
-    //function isEnough()
+    function isEnough(Insuree person, uint payment, string name) private returns(bool){
+        uint carID = person.carsOwned[name].carID;
+        uint expected = tripDistance[carID] * insurancePrice[carID];
+        if (expected > payment){
+            return false;
+        } else {
+            return true;
+        }
+    }
     
+    function payInsurance(Insuree person, string name) public payable returns(string){
+        uint carID = person.carsOwned[name].carID;
+        if (isEnough(person,msg.value,name)){
+            paid[carID] = true;
+            tripDistance[carID] = 0;
+            return "Paid";
+        } else {
+            return "You didn't pay enough!";
+            revert();
+        }
+    }
     
     function(){
         revert();
     }
 }
+contract CarInsurance {
+    
+    enum Location{ longtitute, latitude  }
+    enum ActionChoices { GoLeft, GoRight, GoStraight, GoBack}
+    
+    uint private idGen = 1;
+    uint private carIDGen = 1;
+    
+    struct Car{
+        uint mileage;
+        Insuree owner;
+        bool paid;
+        uint carID;
+    }
+    
+    struct Insuree{
+        mapping(string => Car) carsOwned;
+        address owner;
+        uint ID;
+    }
+    
+    mapping(uint => Insuree) users;
+    mapping(uint => uint) insurance;
+    mapping(uint => uint) insurancePrice;
+    mapping(uint => uint) tripDistance;
+    mapping(uint => bool) paid;
+    
+    modifier onlyBy(address user)
+    {
+        require(msg.sender == user);
+        _;
+    }
+    
+    function startPolicy() returns(string){
+        //this.ID = idGen;
+        idGen += 1;
+    }
+    
+    function registerCar(Insuree person, uint miles, string name) public returns(bool){
+        require(msg.sender == person.owner);
+        Car newCar;
+        newCar.mileage = miles;
+        newCar.owner = person;
+        newCar.paid = true;
+        newCar.carID = carIDGen;
+        carIDGen += 1;
+        person.carsOwned[name] = newCar;
+        return true;
+    }
+    
+    function getPrice(Insuree person, string name) public returns(uint){
+        uint carID = person.carsOwned[name].carID;
+        return insurancePrice[carID];
+    }
+    
+    function getCarID(Insuree person, string name) public returns(uint){
+        return person.carsOwned[name].carI}
 
